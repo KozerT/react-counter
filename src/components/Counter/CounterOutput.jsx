d3478aaa30aa6fdbb004c9c491bb859338e76d0d
KozerT <kozer.vizastaff@gmail.com>
2024-02-06 22:39:09 +0100

    Implemented Header component and styles

---
+++ src/components/Counter/CounterOutput.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CounterOutput = () => {
+  return <div></div>;
+};
+
+export default CounterOutput;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { log } from "../log.js";
 
 const Header = () => {
-  return <div>Text</div>;
+  log("<Header /> rendered", 1);
+  return (
+    <header id="main-header">
+      <h1>React - Behind The Scenes</h1>
+    </header>
+  );
 };
 
 export default Header;
